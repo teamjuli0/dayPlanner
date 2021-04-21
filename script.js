8197e36300b6bc2053aa0ce6ca2817eb244048cd
Julio Valdez <valdezjulio95@gmail.com>
2021-04-20 23:07:33 -0700

    added code for dry html (solution)

--- script.js
+++ script.js
@@ -1,27 +1,58 @@
-var container = document.querySelector('.container');
-var plannerArr = localStorage.getItem('scheduler') || {};
+var container = document.querySelector('.container')
+var plannerArr = localStorage.getItem('plannerArr') || '{}'
+var dt = luxon.DateTime
+var now = dt.now()
 
-var currentTime = 9;
+document.getElementById('currentDay').innerText = now.toLocaleString(
+  dt.DATETIME_MED
+)
+
+var currentTime = 9
 for (var i = 0; i < 8; i++) {
+  var pTime =
+    currentTime === 12
+      ? currentTime + 'PM'
+      : currentTime >= 13
+      ? currentTime - 12 + 'PM'
+      : currentTime + 'AM'
+
   var hourRow = `<div class="row time-block">
-    <p class="col-1 hour"></p>
+    <p class="col-1 hour">${pTime}</p>
     <textarea id='${currentTime}' class="col-10 description"></textarea>
     <button class="col-1 saveBtn"><i class="fas fa-save"></i></button>
-  </div>`;
+  </div>`
 
-  container.innerHTML += hourRow;
+  container.innerHTML += hourRow
 
-  currentTime++;
+  currentTime++
 }
 
 for (var i = 0; i < 8; i++) {
-  var currentRow = document.getElementsByClassName('saveBtn')[i];
-  currentRow.previousElementSibling.classList.add('past');
+  var currentRow = document.getElementsByClassName('saveBtn')[i]
+  var previousEl = currentRow.previousElementSibling
+  var parsedPlanner = JSON.parse(plannerArr)
+
+  switch (true) {
+    case now.hour > JSON.parse(previousEl.id):
+      previousEl.classList.add('past')
+      break
+    case now.hour === JSON.parse(previousEl.id):
+      previousEl.classList.add('present')
+      break
+    default:
+      previousEl.classList.add('future')
+      break
+  }
+
+  if (parsedPlanner[previousEl.id] !== undefined) {
+    previousEl.value = parsedPlanner[previousEl.id]
+  }
+
   currentRow.addEventListener('click', function () {
-    console.log(this.previousElementSibling.id);
-    plannerArr[
+    parsedPlanner[
       this.previousElementSibling.id
-    ] = this.previousElementSibling.innerText;
-    console.log(this.siblings);
-  });
+    ] = this.previousElementSibling.value
+
+    localStorage.setItem('plannerArr', JSON.stringify(parsedPlanner))
+  })
 }
